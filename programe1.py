1d3d1b5dec70a1d52ea3997f1f7f403f6ac423ad
DEEPESH KUMAR SONI <49856347+deepeshkumarsoni@users.noreply.github.com>
2019-04-23 16:25:07 +0530

    Add files via upload

---
+++ programe1.py
@@ -0,0 +1,12 @@
+a=int(input('Enter the 1 No :'))
+b=int(input('Enter the 2 No :'))
+print('The Sum is :',a+b)
+print('The Subtraction is :',a-b)
+print('The Multiplication is :',a*b)
+print('The Division is :',a/b)
+print('The Floor Division is :',a//b)
+print('The Modulus is :',a%b)
+
+
+
+
