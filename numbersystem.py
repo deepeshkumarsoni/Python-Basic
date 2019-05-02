c1f07d8771a20546581976af17f5bc274f58d611
Deepesh Kumar Soni <49856347+deepeshkumarsoni@users.noreply.github.com>
2019-05-02 16:01:53 +0530

    Add files via upload

---
+++ numbersystem.py
@@ -0,0 +1,17 @@
+#Binary Number System
+a=0b1111
+b=0B111
+print(a)
+print(b)
+
+#Octal Number System
+a=0o257
+b=0O576
+print(a)
+print(b)
+
+#Hexadecimal Number System
+a=0x54af
+b=0X78ef
+print(a)
+print(b)
