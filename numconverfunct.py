5ac8a8540fdc19a9a86cfb65bf824ce81cea6447
Deepesh Kumar Soni <49856347+deepeshkumarsoni@users.noreply.github.com>
2019-05-02 23:46:26 +0530

    Add files via upload

---
+++ numconverfunct.py
@@ -0,0 +1,14 @@
+#1.Conversion into Binary Form
+print('Conversion into Binary Form')
+print(bin(15345))
+print(bin(0o7625))
+print(bin(0xface34))
+print('\nConversion into Octal Form')
+#2.Conversion into Octal form
+print(oct(34765))
+print(oct(0b1101110001))
+print(oct(0xafc4f))
+print('\nConversion into Hexadecimal Form')
+print(hex(84579))
+print(hex(0b101011110))
+print(hex(0o7467))
