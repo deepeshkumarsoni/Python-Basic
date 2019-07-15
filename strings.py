ea0a2c22b44575fa4a85f5e70a2d1343aff4c738
Deepesh Kumar Soni <49856347+deepeshkumarsoni@users.noreply.github.com>
2019-07-15 14:30:38 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,24 @@
+# String Programs With All Function and Methods
+print('******** WELCOME TO THE STRING WORLD ********') 
+a='deepesh'
+print(a)
+print(a+' '+ 'soni')   # Concatenating Strings using '+' Operator
+print(a*3)             # Printing Multiple String using '*' Operator
+print(len(a))          # Printing length of String using len()
+print(a.upper())
+print(a[1:7:2])
+# String Support Negative Index
+a='deepesh'
+print(a[-1],a[-2])
+print(a[-5:-1])
+
+# Loop To Print String
+s=input('Enter The String Here :')
+for i in s :
+    print(i)
+
+# Print Reverse String 
+print(s[:])
+print(s[::-1])
+
+# 
